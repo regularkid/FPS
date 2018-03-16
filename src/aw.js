aa5b25fe8d7a50ca2f4edc380b2c77a8accc5ec3
Bryan Perfetto <regularkid@gmail.com>
2018-03-15 22:21:40 -0700

    Initial commit with skeleton engine.

---
+++ src/aw.js
@@ -0,0 +1,39 @@
+let aw = (function()
+{
+    let public = {};
+
+    let m_lastTime = 0;
+  
+    public.init = (width, height, fps, state) =>
+    {
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        canvas.style.backgroundColor = "black";
+        document.getElementById("game").appendChild(canvas);
+
+        public.ctx = canvas.getContext('2d');
+        public.width = width;
+        public.height = height;
+        public.state = state;
+
+        gameLoop();
+    }
+
+    gameLoop = () =>
+    {
+        let curTime = Date.now();
+        let deltaTime = (curTime - m_lastTime) / 1000.0;
+        m_lastTime = curTime;
+
+        public.ctx.clearRect(0, 0, public.width, public.height);
+        if (public.state)
+        {
+            public.state(deltaTime);
+        }
+
+        window.requestAnimationFrame(gameLoop);
+    }
+     
+    return public;
+}());
---
+++ src/main.js
@@ -0,0 +1,22 @@
+aw.init(640, 480, 60, update);
+
+var x = 0;
+var vx = 200.0;
+var size = 50;
+function update(delta)
+{
+    x += vx * delta;
+    if (x < 0.0)
+    {
+        x = 0.0;
+        vx = -vx;
+    }
+    else if (x >= 640 - size)
+    {
+        x = 640 - size;
+        vx = -vx;
+    }
+
+    aw.ctx.fillStyle = "red";
+    aw.ctx.fillRect(x, 10, size, size);
+}
